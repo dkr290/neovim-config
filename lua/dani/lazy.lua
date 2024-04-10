4f967b6fd870cba9d6873390dc8b482dbe785631
Danail <d_krasimirov@yahoo.com>
2024-04-10 22:31:53 +0200

    adding more plugins

--- lua/dani/lazy.lua
+++ lua/dani/lazy.lua
@@ -11,4 +11,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("dani.plugins")
+require("lazy").setup("dani.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+   change_detection = {
+    notify = false,
+  },
+
+})
