dfccf55a30c85f92bb83fbcfb8b9e270d94d8300
Danail <d_krasimirov@yahoo.com>
2024-08-05 12:48:32 +0200

    fixing the plugin

--- lua/dani/plugins/vim-visual.lua
+++ lua/dani/plugins/vim-visual.lua
@@ -3,50 +3,6 @@ return {
 	"mg979/vim-visual-multi",
 	event = "VeryLazy",
 	branch = "master",
-	keys = {
-		{
-			"<leader>mc",
-			function()
-				vim.cmd("call vm#commands#add_cursor_down(0, 0)")
-			end,
-			desc = "Add cursor down",
-		},
-		{
-			"<leader>mC",
-			function()
-				vim.cmd("call vm#commands#add_cursor_up(0, 0)")
-			end,
-			desc = "Add cursor up",
-		},
-		{
-			"<leader>mn",
-			function()
-				vim.cmd("call vm#commands#find_next(0, 0)")
-			end,
-			desc = "Find next occurrence",
-		},
-		{
-			"<leader>mN",
-			function()
-				vim.cmd("call vm#commands#find_prev(0, 0)")
-			end,
-			desc = "Find previous occurrence",
-		},
-		{
-			"<leader>ms",
-			function()
-				vim.cmd("call vm#commands#select_all(0)")
-			end,
-			desc = "Select all occurrences",
-		},
-		{
-			"<leader>mq",
-			function()
-				vim.cmd("call vm#commands#remove_cursor(0, 0)")
-			end,
-			desc = "Remove last cursor",
-		},
-	},
 	config = function()
 		vim.g.VM_maps = {
 			["Find Under"] = "<C-n>",
