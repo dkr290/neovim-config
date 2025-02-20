9dc879a0e7d72dcd55630c203916428a775b442f
dans <dans@bankingcircle.com>
2025-02-20 12:22:55 +0100

    adding illuminate

---
+++ lua/dani/plugins/illuminate.lua
@@ -0,0 +1,23 @@
+return {
+	"RRethy/vim-illuminate",
+	config = function()
+		require("illuminate").configure({
+			delay = 200, -- Delay before highlighting
+			under_cursor = true, -- Highlight word under cursor
+		})
+
+		-- Global Toggle Variable
+		vim.g.illuminate_enabled = true
+
+		-- Toggle Function
+		vim.keymap.set("n", "<leader>m", function()
+			vim.g.illuminate_enabled = not vim.g.illuminate_enabled
+			if vim.g.illuminate_enabled then
+				vim.cmd("IlluminateResume") -- Enable highlighting
+			else
+				vim.cmd("IlluminatePause") -- Disable highlighting
+			end
+			print("Illuminate: " .. (vim.g.illuminate_enabled and "ON" or "OFF"))
+		end, { noremap = true, silent = true })
+	end,
+}
