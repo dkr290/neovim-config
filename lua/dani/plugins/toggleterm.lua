8a11c961e995f5eaef8cd12a2d73e0011e27e17d
Danail <d_krasimirov@yahoo.com>
2025-03-20 22:07:50 +0100

    adding cx for exit and not f4

--- lua/dani/plugins/toggleterm.lua
+++ lua/dani/plugins/toggleterm.lua
@@ -1,115 +1,114 @@
-
 return {
-  -- amongst your other plugins
- 'akinsho/toggleterm.nvim',
-  event = { "BufReadPre", "BufNewFile" },
-  version = "*",
-  -- cmd = "ToggleTerm",
-  build= ":ToggleTerm",
-  lazy = true,
-    cmd = { "ToggleTerm" },
-    keys = {
-      {
-        "<leader>bf",
-        function()
-          local count = vim.v.count1
-          require("toggleterm").toggle(count, 0, vim.loop.cwd(), "float")
-        end,
-        desc = "ToggleTerm (float root_dir)",
-      },
-      {
-        "<leader>bh",
-        function()
-          local count = vim.v.count1
-          require("toggleterm").toggle(count, 15, vim.loop.cwd(), "horizontal")
-        end,
-        desc = "ToggleTerm (horizontal root_dir)",
-      },
-      {
-        "<leader>bv",
-        function()
-          local count = vim.v.count1
-          require("toggleterm").toggle(count, vim.o.columns * 0.4, vim.loop.cwd(), "vertical")
-        end,
-        desc = "ToggleTerm (vertical root_dir)",
-      },
-      {
-        "<leader>bn",
-        "<cmd>ToggleTermSetName<cr>",
-        desc = "Set term name",
-      },
-      {
-        "<leader>bs",
-        "<cmd>TermSelect<cr>",
-        desc = "Select term",
-      },
-      {
-        "<leader>bt",
-        function()
-          require("toggleterm").toggle(1, 100, vim.loop.cwd(), "tab")
-        end,
-        desc = "ToggleTerm (tab root_dir)",
-      },
-      {
-        "<leader>bb",
-        function()
-          require("toggleterm").toggle(1, 100, vim.loop.cwd(), "tab")
-        end,
-        desc = "ToggleTerm (tab cwd_dir)",
-      },
-    },
-    opts = {
-      -- size can be a number or function which is passed the current terminal
-      size = function(term)
-        if term.direction == "horizontal" then
-          return 15
-        elseif term.direction == "vertical" then
-          return vim.o.columns * 0.4
-        end
-      end,
-      open_mapping = [[<F4>]],
-      -- on_open = fun(t: Terminal), -- function to run when the terminal opens
-      -- on_close = fun(t: Terminal), -- function to run when the terminal closes
-      -- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
-      -- on_stderr = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stderr
-      -- on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
-      hide_numbers = true, -- hide the number column in toggleterm buffers
-      shade_filetypes = {},
-      shade_terminals = true,
-      -- shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-      start_in_insert = true,
-      insert_mappings = true, -- whether or not the open mapping applies in insert mode
-      terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-      persist_size = true,
-      direction = "horizontal" or "vertical" or "window" or "float",
-      -- direction = "vertical",
-      close_on_exit = true, -- close the terminal window when the process exits
-      -- shell = vim.o.shell, -- change the default shell
-      -- This field is only relevant if direction is set to 'float'
-      -- float_opts = {
-      --   -- The border key is *almost* the same as 'nvim_open_win'
-      --   -- see :h nvim_open_win for details on borders however
-      --   -- the 'curved' border is a custom border type
-      --   -- not natively supported but implemented in this plugin.
-      --   border = 'single' or 'double' or 'shadow' or 'curved',
-      --   width = <value>,
-      --   height = <value>,
-      --   winblend = 3,
-      --   highlights = {
-      --     border = "Normal",
-      --     background = "Normal",
-      --   }
-      -- }
-    },
-  }
-  -- keys = { { "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
-  -- opts = {
-  --       open_mapping = [[<F4>]],
-  --       direction = "horizontal",
-  --       shade_filetypes = {},
-  --       hide_numbers = true,
-  --       insert_mappings = true,
-  --       terminal_mappings = true,
-  --       start_in_insert = true,
-  --       close_on_exit = true,
-  -- },
+	-- amongst your other plugins
+	"akinsho/toggleterm.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	version = "*",
+	-- cmd = "ToggleTerm",
+	build = ":ToggleTerm",
+	lazy = true,
+	cmd = { "ToggleTerm" },
+	keys = {
+		{
+			"<leader>bf",
+			function()
+				local count = vim.v.count1
+				require("toggleterm").toggle(count, 0, vim.loop.cwd(), "float")
+			end,
+			desc = "ToggleTerm (float root_dir)",
+		},
+		{
+			"<leader>bh",
+			function()
+				local count = vim.v.count1
+				require("toggleterm").toggle(count, 15, vim.loop.cwd(), "horizontal")
+			end,
+			desc = "ToggleTerm (horizontal root_dir)",
+		},
+		{
+			"<leader>bv",
+			function()
+				local count = vim.v.count1
+				require("toggleterm").toggle(count, vim.o.columns * 0.4, vim.loop.cwd(), "vertical")
+			end,
+			desc = "ToggleTerm (vertical root_dir)",
+		},
+		{
+			"<leader>bn",
+			"<cmd>ToggleTermSetName<cr>",
+			desc = "Set term name",
+		},
+		{
+			"<leader>bs",
+			"<cmd>TermSelect<cr>",
+			desc = "Select term",
+		},
+		{
+			"<leader>bt",
+			function()
+				require("toggleterm").toggle(1, 100, vim.loop.cwd(), "tab")
+			end,
+			desc = "ToggleTerm (tab root_dir)",
+		},
+		{
+			"<leader>bb",
+			function()
+				require("toggleterm").toggle(1, 100, vim.loop.cwd(), "tab")
+			end,
+			desc = "ToggleTerm (tab cwd_dir)",
+		},
+	},
+	opts = {
+		-- size can be a number or function which is passed the current terminal
+		size = function(term)
+			if term.direction == "horizontal" then
+				return 15
+			elseif term.direction == "vertical" then
+				return vim.o.columns * 0.4
+			end
+		end,
+		open_mapping = [[<C-x>]],
+		-- on_open = fun(t: Terminal), -- function to run when the terminal opens
+		-- on_close = fun(t: Terminal), -- function to run when the terminal closes
+		-- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
+		-- on_stderr = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stderr
+		-- on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
+		hide_numbers = true, -- hide the number column in toggleterm buffers
+		shade_filetypes = {},
+		shade_terminals = true,
+		-- shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+		start_in_insert = true,
+		insert_mappings = true, -- whether or not the open mapping applies in insert mode
+		terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+		persist_size = true,
+		direction = "horizontal" or "vertical" or "window" or "float",
+		-- direction = "vertical",
+		close_on_exit = true, -- close the terminal window when the process exits
+		-- shell = vim.o.shell, -- change the default shell
+		-- This field is only relevant if direction is set to 'float'
+		-- float_opts = {
+		--   -- The border key is *almost* the same as 'nvim_open_win'
+		--   -- see :h nvim_open_win for details on borders however
+		--   -- the 'curved' border is a custom border type
+		--   -- not natively supported but implemented in this plugin.
+		--   border = 'single' or 'double' or 'shadow' or 'curved',
+		--   width = <value>,
+		--   height = <value>,
+		--   winblend = 3,
+		--   highlights = {
+		--     border = "Normal",
+		--     background = "Normal",
+		--   }
+		-- }
+	},
+}
+-- keys = { { "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
+-- opts = {
+--       open_mapping = [[<F4>]],
+--       direction = "horizontal",
+--       shade_filetypes = {},
+--       hide_numbers = true,
+--       insert_mappings = true,
+--       terminal_mappings = true,
+--       start_in_insert = true,
+--       close_on_exit = true,
+-- },
