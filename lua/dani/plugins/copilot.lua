e75de736729439e8ecd6025d4c9ef23630b660db
dans <dans@bankingcircle.com>
2025-04-03 13:03:52 +0200

    fix compilot

--- lua/dani/plugins/copilot.lua
+++ lua/dani/plugins/copilot.lua
@@ -3,13 +3,13 @@ return {
 	-- disable copilot by default
 	lazy = true,
 	keys = { "<leader>cp" },
+
 	config = function()
-		vim.cmd("Copilot setup")
-		print("Copilot setup 🤖")
 		vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
 			expr = true,
 			replace_keycodes = false,
 		})
+
 		vim.keymap.set("n", "<leader>cd", function()
 			vim.cmd(":Copilot disable")
 			print("Copilot deactivated 🔥")
