d43f3dca7668527a1713c2e99d861dee7d3b5338
dans <dans@bankingcircle.com>
2025-04-03 15:43:50 +0200

    adding dm for diff offiles

--- lua/dani/core/keymaps.lua
+++ lua/dani/core/keymaps.lua
@@ -30,3 +30,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Map leader + d to diff two files in a vertical split
+-- Diff file management with leader key
+keymap.set("n", "<Leader>dm", ":diffthis<CR>", { noremap = true, silent = true }) -- Open the vertical split
