a921898456a86a66189acbdf5475d761e8ed5aa0
dans <dans@bankingcircle.com>
2025-04-30 10:13:39 +0200

    adding setup with leader cj

--- lua/dani/plugins/copilot.lua
+++ lua/dani/plugins/copilot.lua
@@ -10,6 +10,13 @@ return {
 			replace_keycodes = false,
 		})
 
+		vim.keymap.set("n", "<leader>cj", function()
+			vim.cmd(":Copilot setup")
+			print("Manually triggered Copilot setup.")
+		end, {
+			desc = "Manually trigger Copilot setup",
+		})
+
 		vim.keymap.set("n", "<leader>cd", function()
 			vim.cmd(":Copilot disable")
 			print("Copilot deactivated 🔥")
