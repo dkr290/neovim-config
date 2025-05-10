fb9d1ec7d3334f1ab897dd29914f3039103fb70d
Danail <d_krasimirov@yahoo.com>
2025-05-10 23:40:39 +0200

    add more on go for lsp

--- lua/dani/plugins/lsp/lspconfig.lua
+++ lua/dani/plugins/lsp/lspconfig.lua
@@ -135,6 +135,9 @@ return {
 								completion = {
 									callSnippet = "Replace",
 								},
+								hint = {
+									enable = true,
+								},
 							},
 						},
 					})
@@ -236,11 +239,35 @@ return {
 							gopls = {
 								completeUnimported = true, -- Key setting for unimported completions
 								usePlaceholders = true,
+								codelenses = {
+									gc_details = false,
+									generate = true,
+									regenerate_cgo = true,
+									run_govulncheck = true,
+									test = true,
+									tidy = true,
+									upgrade_dependency = true,
+									vendor = true,
+								},
+								hints = {
+									assignVariableTypes = true,
+									compositeLiteralFields = true,
+									compositeLiteralTypes = true,
+									constantValues = true,
+									functionTypeParameters = true,
+									parameterNames = true,
+									rangeVariableTypes = true,
+								},
 								analyses = {
+									nilness = true,
 									unusedparams = true,
+									unusedwrite = true,
+									useany = true,
 								},
 								staticcheck = true,
 								gofumpt = true,
+								directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+								semanticTokens = true,
 							},
 						},
 					})
