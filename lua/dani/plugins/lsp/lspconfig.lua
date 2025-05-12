551fed037aefaecdd2c3ecc252680bfb8b803a06
dans <dans@bankingcircle.com>
2025-05-12 11:56:21 +0200

    move inlay hint to lsp

--- lua/dani/plugins/lsp/lspconfig.lua
+++ lua/dani/plugins/lsp/lspconfig.lua
@@ -1,7 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
-	opts = { inlay_hints = { enabled = true } },
 	dependencies = {
 		"saghen/blink.cmp",
 		"mason-org/mason-lspconfig.nvim",
@@ -89,7 +88,15 @@ return {
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
-
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local bufnr = ev.buf
+				-- Enable native inlay hints
+				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr }) -- :contentReference[oaicite:2]{index=2}
+				-- ... (existing keymaps) ...
+			end,
+		})
 		mason_lspconfig.setup({
 			ensure_installed = {},
 			automatic_enable = true,
