6237fc5635708f9c6738677a632c20309c601846
dans <dans@bankingcircle.com>
2025-06-06 16:50:39 +0200

    return for completion the origianl one

--- lua/dani/plugins/nvim-cmp.lua
+++ lua/dani/plugins/nvim-cmp.lua
@@ -43,7 +43,7 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-s>"] = cmp.mapping.complete(),
+				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
 			}),
