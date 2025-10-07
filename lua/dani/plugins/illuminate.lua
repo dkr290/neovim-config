7d16b0a7e535f95421a5ec9af8b6221f20b4e0a7
Danail <d_krasimirov@yahoo.com>
2025-10-07 21:21:42 +0200

    better which key and illuminate

--- lua/dani/plugins/illuminate.lua
+++ lua/dani/plugins/illuminate.lua
@@ -5,6 +5,10 @@ return {
 			delay = 200, -- Delay before highlighting
 			under_cursor = true, -- Highlight word under cursor
 		})
+		-- Custom Highlights for visibility
+		vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+		vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
 
 		-- Global Toggle Variable
 		vim.g.illuminate_enabled = false
@@ -18,6 +22,6 @@ return {
 				vim.cmd("IlluminatePause") -- Disable highlighting
 			end
 			print("Illuminate: " .. (vim.g.illuminate_enabled and "ON" or "OFF"))
-		end, { noremap = true, silent = true })
+		end, { noremap = true, silent = true, desc = "Toggle word illumination" })
 	end,
 }
--- lua/dani/plugins/which-key.lua
+++ lua/dani/plugins/which-key.lua
@@ -1,13 +1,15 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	opts = {
+		preset = "helix", -- Show the menu as a vertical bar on the left side
+
+		-- Optional: Other good defaults
+		delay = 100, -- Shorter delay (ms) after pressing a prefix key
+		show_keys = true, -- Show the key itself in the group title  },
+	},
 }
