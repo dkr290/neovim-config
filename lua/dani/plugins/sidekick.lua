a4e3a33fd3e8d0a948d9662bb411b4e3fc15ae40
Danail Surudzhiyski <dans@bankingcircle.com>
2026-07-07 17:06:14 +0200

    fix: withoutzellij

--- lua/dani/plugins/sidekick.lua
+++ lua/dani/plugins/sidekick.lua
@@ -2,12 +2,6 @@ return {
   "folke/sidekick.nvim",
   opts = {
     -- add any options here
-    cli = {
-      mux = {
-        backend = "zellij",
-        enabled = true,
-      },
-    },
   },
   keys = {
     {
